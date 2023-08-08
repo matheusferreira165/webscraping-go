57bfbf4175e06470c60e6c77a7820a54be342b18
Matheus FErreira <matheus.ferreira165@outlook.com>
2023-08-07 22:39:02 -0300

    Adicionado outros metodos para teste

--- main.go
+++ main.go
@@ -2,20 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gocolly/colly"
 )
 
 func main() {
 
-	c := colly.NewCollector(
-		colly.AllowedDomains("en.wikipedia.org"),
-	)
+	c := colly.NewCollector()
 
-	c.OnHTML(".mw-parser-output", func(e *colly.HTMLElement) {
-		links := e.ChildAttrs("a", "href")
-		fmt.Println(links)
+	//Methods Tests
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visitando o site ", r.URL)
 	})
 
-	c.Visit("https://en.wikipedia.org/wiki/Web_scraping")
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Algo deu errado", err)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Pagina visitada: ", r.Request.URL)
+	})
+
+	c.OnHTML("a", func(e *colly.HTMLElement) {
+		fmt.Printf("%v", e.Attr("href"))
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println(r.Request.URL, "Scraped")
+	})
+	c.Visit("https://br.wikipedia.org/wiki/Degemer")
+
 }
