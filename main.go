008f4d23ce2a3704c06a7652a1a220b43a6587d1
Matheus FErreira <matheus.ferreira165@outlook.com>
2023-08-08 21:26:35 -0300

    Inicializado e criando slice com  os valores do objeto pokemon

--- main.go
+++ main.go
@@ -1,38 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/gocolly/colly"
 )
 
 func main() {
+	var pokemonProducts []PokemonProduct
 
 	c := colly.NewCollector()
 
-	//Methods Tests
+	c.Visit("https://scrapeme.live/shop/")
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visitando o site ", r.URL)
+	c.OnHTML("li.product", func(e *colly.HTMLElement) {
+		pokemonProduct := PokemonProduct{}
+
+		pokemonProduct.url = e.ChildAttr("a", "href")
+		pokemonProduct.image = e.ChildAttr("img", "src")
+		pokemonProduct.name = e.ChildText("h2")
+		pokemonProduct.price = e.ChildText(".price")
+
+		pokemonProducts = append(pokemonProducts, pokemonProduct)
 	})
-
-	c.OnError(func(_ *colly.Response, err error) {
-		log.Println("Algo deu errado", err)
-	})
-
-	c.OnResponse(func(r *colly.Response) {
-		fmt.Println("Pagina visitada: ", r.Request.URL)
-	})
-
-	c.OnHTML("a", func(e *colly.HTMLElement) {
-		fmt.Printf("%v", e.Attr("href"))
-	})
-
-	c.OnScraped(func(r *colly.Response) {
-		fmt.Println(r.Request.URL, "Scraped")
-	})
-
-	c.Visit("https://br.wikipsdasdedia.org/wiki/Degemer")
 
 }
