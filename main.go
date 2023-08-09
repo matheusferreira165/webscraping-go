27530673cef2a248a7b81a2590312f5045bae797
Matheus FErreira <matheus.ferreira165@outlook.com>
2023-08-08 22:09:20 -0300

    Adicionado a estrutura, criado o csv com os dados do scrapper

--- main.go
+++ main.go
@@ -1,25 +1,63 @@
 package main
 
 import (
+	"encoding/csv"
+	"log"
+	"os"
+
 	"github.com/gocolly/colly"
 )
 
+type PokemonProduct struct {
+	Url, Image, Name, Price string
+}
+
 func main() {
+
 	var pokemonProducts []PokemonProduct
 
 	c := colly.NewCollector()
 
-	c.Visit("https://scrapeme.live/shop/")
-
 	c.OnHTML("li.product", func(e *colly.HTMLElement) {
+
 		pokemonProduct := PokemonProduct{}
 
-		pokemonProduct.url = e.ChildAttr("a", "href")
-		pokemonProduct.image = e.ChildAttr("img", "src")
-		pokemonProduct.name = e.ChildText("h2")
-		pokemonProduct.price = e.ChildText(".price")
+		pokemonProduct.Url = e.ChildAttr("a", "href")
+		pokemonProduct.Image = e.ChildAttr("img", "src")
+		pokemonProduct.Name = e.ChildText("h2")
+		pokemonProduct.Price = e.ChildText(".price")
 
 		pokemonProducts = append(pokemonProducts, pokemonProduct)
 	})
 
+	c.Visit("https://scrapeme.live/shop/")
+
+	file, err := os.Create("products.csv")
+	if err != nil {
+		log.Fatalln("Falha para criar o arquivo CSV", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+
+	headers := []string{
+		"url",
+		"image",
+		"name",
+		"price",
+	}
+
+	writer.Write(headers)
+
+	for _, pokemonProduct := range pokemonProducts {
+		record := []string{
+			pokemonProduct.Url,
+			pokemonProduct.Image,
+			pokemonProduct.Name,
+			pokemonProduct.Price,
+		}
+
+		writer.Write(record)
+	}
+	defer writer.Flush()
 }
